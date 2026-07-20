53e7a4e85880624ccc972e46a46e273f79027f70
7zxy <zxy38107@gmail.com>
2026-07-20 06:03:16 +0530

    Update UI and game version in 7zxy Hub

--- 7zxy hub.lua
+++ 7zxy hub.lua
@@ -1,6 +1,6 @@
 --[[
     7zxy Hub - 1+ Shrink Per Step
-    Rewrote UI and updated to current game version.
+    Rewrote UI and updated to latest game version
     ═══════════════════════════════════════════════════════
 ]]
 
